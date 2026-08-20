6147eb70cc263cc0f8e1257f8f3d3d84b92119c9
akanbiyinka <113189266+akanbiyinka@users.noreply.github.com>
2026-08-20 15:56:01 -0400

    AAA

--- app/src/app.py
+++ app/src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify
 from kubernetes_client import get_cluster_data
-import socket
 
 
 app = Flask(__name__)
@@ -9,41 +8,36 @@ app = Flask(__name__)
 @app.route("/")
 def dashboard():
 
-    pods = get_cluster_data()
-
-    running = len(
-        [
-            p for p in pods
-            if p["status"] == "Running"
-        ]
-    )
+    data = get_cluster_data()
 
     return render_template(
         "index.html",
-        pods=pods,
-        running=running,
-        hostname=socket.gethostname()
+        data=data
+    )
+
+
+@app.route("/api/cluster")
+def cluster():
+
+    return jsonify(
+        get_cluster_data()
     )
 
 
 @app.route("/health")
 def health():
 
-    return jsonify(
-        {
-            "status":"healthy"
-        }
-    )
+    return {
+        "status":"healthy"
+    }
 
 
 @app.route("/ready")
 def ready():
 
-    return jsonify(
-        {
-            "status":"ready"
-        }
-    )
+    return {
+        "status":"ready"
+    }
 
 
 if __name__ == "__main__":
--- app/src/kubernetes_client.py
+++ app/src/kubernetes_client.py
@@ -31,3 +31,44 @@ def get_cluster_data():
         )
 
     return pod_data
+from kubernetes import client, config
+
+
+def load_cluster():
+
+    try:
+        config.load_incluster_config()
+    except:
+        config.load_kube_config()
+
+
+def get_cluster_data():
+
+    load_cluster()
+
+    v1 = client.CoreV1Api()
+    apps = client.AppsV1Api()
+    autoscale = client.AutoscalingV2Api()
+
+
+    nodes = v1.list_node()
+
+    pods = v1.list_namespaced_pod(
+        namespace="openshift-enterprise-platform"
+    )
+
+    deployments = apps.list_namespaced_deployment(
+        namespace="openshift-enterprise-platform"
+    )
+
+    hpa = autoscale.list_namespaced_horizontal_pod_autoscaler(
+        namespace="openshift-enterprise-platform"
+    )
+
+
+    return {
+        "nodes": len(nodes.items),
+        "pods": len(pods.items),
+        "deployments": len(deployments.items),
+        "hpa": len(hpa.items)
+    }
