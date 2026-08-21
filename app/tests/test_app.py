8b3fc7431be66b8c35310a3d6691a46e76073c50
akanbiyinka <113189266+akanbiyinka@users.noreply.github.com>
2026-08-21 14:23:42 -0400

    Update test_app.py

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,6 +1,22 @@
+from unittest.mock import patch
 from src.app import app
 
-def test_home():
+
+MOCK_CLUSTER_DATA = {
+    "nodes": 1,
+    "pods": {
+        "running": 2,
+        "total": 2
+    },
+    "deployments": 1,
+    "services": 1,
+    "hpa": 1
+}
+
+
+@patch("src.app.get_cluster_data")
+def test_home(mock_get_cluster_data):
+    mock_get_cluster_data.return_value = MOCK_CLUSTER_DATA
 
     client = app.test_client()
 
@@ -8,16 +24,16 @@ def test_home():
 
     assert response.status_code == 200
 
-def test_health():
 
+def test_health():
     client = app.test_client()
 
     response = client.get("/health")
 
     assert response.status_code == 200
 
-def test_ready():
 
+def test_ready():
     client = app.test_client()
 
     response = client.get("/ready")
